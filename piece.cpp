4c577ff01c5e30081b8e790d149bfb3b12b8a665
robertbaranov <rbaranov@uvm.edu>
2018-04-24 16:40:57 -0400

    Changed main

--- piece.cpp
+++ piece.cpp
@@ -3,40 +3,146 @@
 //
 
 #include "piece.h"
+#include "graphics.h"
 
-
-Piece::Piece() {
-    color = " ";
-    location = 0;
+Shape::Shape() {
+    // default color: black
+    fill = {0, 0, 0};
+    position = {0, 0};
 }
 
-Piece::Piece(string c, int loc) {
-    color = c;
-    location = loc;
+Shape::Shape(hue f) : Shape() {
+    fill = f;
 }
 
-Piece::~Piece() {
-
+Shape::Shape(xy_position p) : Shape() {
+    set_position(p);
 }
-string Piece::get_color() const {
+
+Shape::Shape(hue f, xy_position p) : Shape(f) {
+        set_position(p);
+}
+
+Shape::Shape(hue f, int x_in, int y_in) : Shape(f) {
+        set_position(x_in, y_in);
+}
+
+Shape::Shape(double x_in, double y_in) : Shape() {
+    set_position(x_in, y_in);
+}
+
+Shape::Shape(const Shape &s) : fill(s.fill) {
+}
+
+Shape::~Shape() {
+    // does nothing
+}
+string Shape::get_color() const {
     return color;
+}
+
+double Shape::get_area() const {
+    return calculate_area();
+}
+
+double Shape::get_perimeter() const {
+    return calculate_perimeter();
+}
+
+hue Shape::get_fill() const {
+    return fill;
+}
+
+xy_position Shape::get_position() const {
+    return position;
+}
+
+double Shape::get_x() const {
+    return position.x;
+}
+
+double Shape::get_y() const {
+    return position.y;
+}
+void Shape::set_color(string piece_color) {
+    color = piece_color;
+}
+
+void Shape::set_fill(hue f) {
+fill = f;
+}
+
+void Shape::set_fill(double r, double g, double b) {
+    fill = {r, g, b};
+}
+
+void Shape::set_position(xy_position p) {
+    position = p;
+}
+
+void Shape::set_position(double x_in, double y_in) {
+    position = {x_in, y_in};
+}
+
+void Shape::move(int delta_x, int delta_y) {
+    // Note: if delta_x is negative, this will
+    // in effect subtract from the x position.
+    position.x += delta_x;
+    // Note: if delta_y is negative, this will
+    // in effect subtract from the y position.
+    position.y += delta_y;
+}
+
+Piece::Piece() : Shape(), radius(0) {
+}
+
+Piece::Piece(double r) : Shape() {
+    set_radius(r);
+}
+
+Piece::Piece(double r, hue f) : Shape(f) {
+    set_radius(r);
+}
+
+Piece::Piece(hue f) : Shape(f), radius(0) {
+}
+
+Piece:: ~Piece() {
+    // does nothing
+}
+
+double Piece::get_radius() const {
+    return radius;
+}
+
+void Piece::set_radius(double r) {
+    if (r < 0) {
+        radius = 0;
+    } else {
+        radius = r;
+    }
+}
+
+void Piece::draw() const {
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(fill.red, fill.green, fill.blue);
+    glVertex2i(position.x, position.y);
+    for (double i = 0; i <= 2.0*M_PI+0.05; i += 2.0*M_PI/360.0) {
+        glVertex2i(position.x+radius*cos(i),position.y+radius*sin(i));
+    }
+    glEnd();
 
 }
 
-int Piece::get_location() const {
-    return location;
+double Piece::calculate_area() const {
+    return M_PI * radius * radius;
 }
 
-void Piece::set_color(string c) {
-    color = c;
+double Piece::calculate_perimeter() const {
+    return 2.0 * M_PI * radius;
 }
 
-void Piece::set_location(int loc) {
-    location = loc;
-}
 
-void Piece::move(int delta_spaces) {
-    location = location + delta_spaces;
-}
 
 
--- piece.h
+++ piece.h
@@ -6,31 +6,94 @@
 #define UNTITLED5_PIECE_H
 
 #include <iostream>
+#include <math.h>
 #include <string>
+#include <memory>
 #include <vector>
+#include "piece.h"
 
 
 using namespace std;
+struct hue{
+    double red;
+    double green;
+    double blue;
+};
+struct xy_position{
+    double x;
+    double y;
+};
+class Shape{
 
-class Piece{
 private:
-    string color;
-    int location;
 public:
+    //Constructors for Shape parent class
+    Shape();
+    Shape(hue f);
+    Shape(xy_position p);
+    Shape(string piece_color);
+    Shape(double x_in, double y_in);
+    Shape(hue f, xy_position p);
+    Shape(hue f, int x_in, int y_in);
+    Shape(const Shape &s); // copy constructor
+
+    ~Shape();
+
+    //getters
+    string get_color() const;
+    double get_area() const;
+    double get_perimeter() const;
+    hue get_fill() const;
+    xy_position get_position() const;
+    double get_x() const;
+    double get_y() const;
+
+    //setters
+    void set_color(string piece_color);
+    void set_fill(hue f);
+    void set_fill(double r, double g, double b);
+    void set_position(xy_position p);
+    void set_position(double x_in, double y_in);
+
+    // methods to move the shape and draw it in the gui
+    void move(int delta_x, int delta_y);
+
+    virtual void draw() const = 0;
+
+protected:
+    hue fill;
+    xy_position position;
+    string color;
+    virtual double calculate_area() const = 0;
+    virtual double calculate_perimeter() const = 0;
+};
+
+class Piece : public Shape {
+public:
+    // Constructors
     Piece();
-    Piece(string c, int loc);
+    Piece(double r);
+    Piece(double r, hue f);
+    Piece(hue f);
+
     ~Piece();
 
+    // getter
+    double get_radius() const;
 
+    // setter
+    void set_radius(double r);
 
-    string get_color() const;
-    int get_location() const;
-
-    void set_color(string c);
-    void set_location(int loc);
-
-    void move(int delta_spaces);
-
+    void draw() const override;
+private:
+    double calculate_area() const override;
+    double calculate_perimeter() const override;
+    double radius;
 };
+
+
+
+
+
 
 #endif //UNTITLED5_PIECE_H
